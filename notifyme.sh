0b349d8365d8858bb57534ac99b8b48eed1c43cf
Miguel Soranno <msoranno@gmail.com>
2018-06-30 20:15:39 +0200

    introducing time

--- notifyme.sh
+++ notifyme.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-
+NOW=`date '+%Y-%m-%d %H:%M:%S'`
 myIP=`curl -s ipecho.net/plain`
 status=$?
 if [ $status -gt 0 ]; then
-	echo "Something wrong"
+	echo "$NOW -- Something wrong"
 else
 	if [ -f current.txt ]; then
 		currentIp=`cat current.txt`
 		if [ $myIP == $currentIp ]; then
-			echo "Same ip. Nothing to do -- $myIP"
+			echo "$NOW -- Same ip. Nothing to do -- $myIP"
 		else
-			echo "Ip has changed... sending notification"
+			echo "$NOW -- Ip has changed- $myIP  ... sending notification"
 			mail -s "Ha cambiado la ip" $1 <<< "Ahora es: $myIP"
 		fi
 	else
-		echo "first execution"
+		echo "$NOW -- first execution"
 		echo $myIP > current.txt
 		mail -s "First exec..Ha cambiado la ip" $1 <<< "Ahora es: $myIP"
 	fi
